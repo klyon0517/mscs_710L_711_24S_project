1baca88010a783227ead97f6c297e836d31309cf
unknown <klyon0517@gmail.com>
2024-01-29 04:43:18 +0000

    Practice - set up a button using an onclick function to fetch the json formatted metrics..

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!--  Metrics Project
 
-      * Software: MSCS_710L_711_24S
+      * Software: Project
+      * Marist Class: MSCS_710L_711_24S
       * Filename: index.php
       * Author: Kerry Lyon
       * Created: Janurary 28, 2024
@@ -38,19 +39,27 @@
 		<script src="https://kit.fontawesome.com/a0ae6b9fc2.js" crossorigin="anonymous"></script>
     
     <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
     <!-- Chart.js -->
     
   </head>
   <body>
-  
-    <h1>
-      <i class="fa-solid fa-rocket"></i>
-      MSCS_710L_711_24S PROJECT
-    </h1>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">
+          <i class="fa-solid fa-rocket"></i>
+          MSCS_710L_711_24S PROJECT
+        </span>
+      </div>
+    </nav>
+    <button type="button" class="btn btn-primary" onclick="getMetrics()">Metrics</button>
+    <div id="metrics" class="container-lg">Performance?</div>
+
     
     <!-- Custom Script -->   
 		<script type="text/javascript" src="scripts/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
   </body>
 </html>
---
+++ scripts/main.js
@@ -0,0 +1,56 @@
+/*  Metrics Project
+
+    * Software: Project
+    * Marist Class: MSCS_710L_711_24S
+    * Filename: main.js
+    * Author: Kerry Lyon
+    * Created: January 28, 2024
+
+    * This file contains the main JavaScript functions.
+  
+*/
+
+window.addEventListener("load", function() {
+  
+  
+
+});
+
+
+
+/*  getMetrics
+*
+*   Creates a basic user profile and sets the 
+*   travel policy to accepted ("y").
+*
+*   parameters:
+*       display -- plugin json
+*  
+*   return value: json response
+*
+*/
+
+function getMetrics() {
+  
+  /* let display = {
+    "plugin" : "complete",
+    "json" : ""
+  }; */
+  
+  /* let options = {
+    method: 'POST',
+    headers: {
+      'Content-Type':
+        'application/json'
+    },
+    body: JSON.stringify(display)
+  }; */
+  
+  fetch("http://localhost/phpsysinfo/xml.php?plugin=complete&json")
+  // fetch("../phpsysinfo/xml.php", options)
+  .then(x => x.text())
+  .then(y => document.getElementById("metrics").innerHTML = y);
+  // Once response is received run another fetch
+  // POST with the parsed json to be saved to the DB
+  
+};
