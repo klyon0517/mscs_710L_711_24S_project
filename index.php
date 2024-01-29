aad24d726582e9a0a3c0e9f995597a9ea0287199
unknown <klyon0517@gmail.com>
2024-01-29 03:15:05 +0000

    Initial upload of app files.

---
+++ index.php
@@ -0,0 +1,56 @@
+<!--  Metrics Project
+
+      * Software: MSCS_710L_711_24S
+      * Filename: index.php
+      * Author: Kerry Lyon
+      * Created: Janurary 28, 2024
+
+      * This file contains the main content layout for the metrics project app.
+
+-->
+
+<?php
+
+  ini_set("error_log", "logs/php_error.log");
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+  
+    <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Metric Project" />
+    <meta name="keywords" content="metrics, computer, marist, class, cpu, memory" />
+    <meta name="author" content="klyon" />
+		
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		<title>Metrics Project</title>
+				
+		<!-- CSS -->
+		<!-- <link rel="stylesheet" type="text/css" href="css/styles.css"> -->
+		
+		<!-- Google Font -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		
+		<!-- Font Awesome -->
+		<script src="https://kit.fontawesome.com/a0ae6b9fc2.js" crossorigin="anonymous"></script>
+    
+    <!-- Bootstrap -->
+    
+    <!-- Chart.js -->
+    
+  </head>
+  <body>
+  
+    <h1>
+      <i class="fa-solid fa-rocket"></i>
+      MSCS_710L_711_24S PROJECT
+    </h1>
+    
+    <!-- Custom Script -->   
+		<script type="text/javascript" src="scripts/main.js"></script>
+    
+  </body>
+</html>
