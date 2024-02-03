bf038559b8b9def930602fa73dcd02219fcf53b5
unknown <klyon0517@gmail.com>
2024-02-03 21:10:36 +0000

    UI concept fleshed out.

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <meta name="author" content="klyon" />
 		
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-		<title>Metrics Project</title>
+		<title>WebServ Metrics</title>
 				
 		<!-- CSS -->
 		<!-- <link rel="stylesheet" type="text/css" href="css/styles.css"> -->
@@ -37,19 +37,135 @@
             
   </head>
   <body>
-    <nav class="navbar bg-body-tertiary">
+    <nav class="navbar bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
       <div class="container-fluid">
         <span class="navbar-brand mb-0 h1">
-          <i class="fa-solid fa-rocket"></i>
-          MSCS_710L_711_24S PROJECT
+          <i class="fa-solid fa-chart-simple me-2"></i>
+          WebServ Metrics
         </span>
       </div>
     </nav>
-    <button type="button" class="btn btn-primary" onclick="getMetrics()">Metrics</button>
-    <div id="metrics" class="container-lg">Performance?</div>
-    <div style="height: 200px; width: 200px">
-      <canvas id="myChart"></canvas>
+    <div class="container-fluid fs-6 font-monospace text-white text-center bg-primary py-3">
+      <div class="row">
+        <div id="hostname" class="col-sm">
+          <i class="fa-solid fa-server"></i>
+          EC2AMAZ-1HQHP5U
+        </div>
+        <div id="distro" class="col-sm">
+          <i class="fa-solid fa-floppy-disk"></i>
+          Microsoft Windows Server 2022 Datacenter
+        </div>
+        <div id="kernal" class="col-sm">
+          <i class="fa-solid fa-microchip"></i>
+          10.0.20348.2227 (64-bit) x64
+        </div>
+        <div id="uptime" class="col-sm">
+          <i class="fa-regular fa-circle-up"></i>
+          5 days 10 hrs 35 mins
+        </div>
+      </div>
     </div>
+    <div class="container-fluid fs-6 bg-secondary text-white text-center py-3">
+      <div class="row">
+        <div id="networkName" class="col-sm">
+          <i class="fa-solid fa-network-wired"></i>
+          AWS PV Network Device #0
+        </div>
+        <div id="networkIpAddress" class="col-sm">
+          <i class="fa-solid fa-ethernet"></i>
+          172.31.25.238
+        </div>
+        <div id="networkReceived" class="col-sm">
+          <i class="fa-solid fa-arrow-down-short-wide"></i>
+          928.70 MiB
+        </div>
+        <div id="networkSent" class="col-sm">
+          <i class="fa-solid fa-arrow-up-from-bracket"></i>
+          438.99 MiB
+        </div>
+      </div>
+    </div>
+    <!-- <div class="container-fluid fs-6 text-center py-3">
+      <div class="row">
+        <div class="col-sm">
+          Current
+        </div>
+        <div class="col-sm">
+          15 mins
+        </div>
+        <div class="col-sm">
+          1 hr
+        </div>
+        <div class="col-sm">
+          1 day
+        </div>
+      </div>
+    </div> -->
+    <div class="container-fluid mt-3">
+      <div class="row">
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="currentCpuLoad" class="border p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="minCpuLoad" class="p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="hourCpuLoad" class="p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="dayCpuLoad" class="p-3"></canvas>
+        </div>
+      </div>    
+    </div>
+    <div class="container-fluid mt-3">
+      <div class="row">
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="currentMemoryUsage" class="border p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="minMemoryUsage" class="p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="hourMemoryUsage" class="p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="dayMemoryUsage" class="p-3"></canvas>
+        </div>
+      </div>    
+    </div>
+    <div class="container-fluid mt-3">
+      <div class="row">
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="currentStorageUsage" class="border p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="minStorageUsage" class="p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="hourStorageUsage" class="p-3"></canvas>
+        </div>
+        <div class="col-sm d-flex justify-content-center" style="height: 200px; width: 200px">
+          <canvas id="dayStorageUsage" class="p-3"></canvas>
+        </div>
+      </div>    
+    </div>
+    <div class="container-fluid text-end fs-6 font-monospace fixed-bottom bg-primary-subtle py-2">
+      MSCS_710L_711_24S PROJECT Authors: Kerry Lyon & Rorie Reyes
+    </div>
+    
+    
+    <!-- <nav class="navbar fixed-bottom bg-body-tertiary" style="background-color: #e3f2fd;">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0">
+          MSCS_710L_711_24S PROJECT
+        </span>
+      </div>
+    </nav> --> 
+    
+    
+    <!-- <div style="height: 200px; width: 200px">
+      <canvas id="myChart"></canvas>
+    </div> -->
 
     
     <!-- Custom Script -->   
--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,8 @@ window.addEventListener("load", function() {
   // run script every 15 secons for first hour
   // then less and less as time goes on
   // setInterval / clearInterval
+  
+  getMetrics();
 
 });
 
@@ -35,7 +37,7 @@ window.addEventListener("load", function() {
 async function getMetrics() {
   
   // const response = await fetch("http://3.135.19.80/phpsysinfo/xml.php?plugin=complete&json");
-  const response = await fetch("http://localhost/phpsysinfo/xml.php?plugin=complete&json");
+  /* const response = await fetch("http://localhost/phpsysinfo/xml.php?plugin=complete&json");
   const metrics = await response.json();
   
   let hostname = metrics["Vitals"]["@attributes"].Hostname;
@@ -50,9 +52,9 @@ async function getMetrics() {
     "<br><br>" +
     "Load: " + cpuLoad +
     "<br><br>" +
-    "Remaining: " + unusedCpuLoad;
+    "Remaining: " + unusedCpuLoad; */
   
-  const ctx = document.getElementById('myChart');
+  /* const ctx = document.getElementById('myChart');
    
   new Chart(ctx, {
     type: 'pie',
@@ -74,14 +76,308 @@ async function getMetrics() {
     options: {
       responsive: true
     }
+  }); */
+  
+  // Current CPU
+  const ctx1 = document.getElementById('currentCpuLoad');
+  
+  new Chart(ctx1, {
+    type: 'pie',
+    data: {
+      labels: [
+        'Current CPU Load'
+      ],
+      datasets: [{
+        label: 'Load%',
+        data: [25, 75],
+        backgroundColor: [
+          'rgba(25, 135, 84, 1.0)',
+          'rgba(185, 185, 185, 1.0)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
   });
   
-  const data = [];
+  // Historical CPU 15 min
+  const ctx2 = document.getElementById('minCpuLoad');
+  
+  new Chart(ctx2, {
+    type: 'pie',
+    data: {
+      labels: [
+        '15 min avg. CPU Load'
+      ],
+      datasets: [{
+        label: 'Load%',
+        data: [35, 65],
+        backgroundColor: [
+          'rgba(255, 193, 7, 0.85)',
+          'rgba(185, 185, 185, 0.85)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Historical CPU 1 hr
+  const ctx3 = document.getElementById('hourCpuLoad');
+  
+  new Chart(ctx3, {
+    type: 'pie',
+    data: {
+      labels: [
+        '1 hr avg. CPU Load'
+      ],
+      datasets: [{
+        label: 'Load%',
+        data: [15, 85],
+        backgroundColor: [
+          'rgba(25, 135, 84, 0.7)',
+          'rgba(185, 185, 185, 0.7)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Historical CPU 1 day
+  const ctx4 = document.getElementById('dayCpuLoad');
+  
+  new Chart(ctx4, {
+    type: 'pie',
+    data: {
+      labels: [
+        '1 day avg. CPU Load'
+      ],
+      datasets: [{
+        label: 'Load%',
+        data: [5, 95],
+        backgroundColor: [
+          'rgba(25, 135, 84, 0.55)',
+          'rgba(185, 185, 185, 0.55)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Current Memory
+  const ctx5 = document.getElementById('currentMemoryUsage');
+  
+  new Chart(ctx5, {
+    type: 'pie',
+    data: {
+      labels: [
+        'Current Memory Usage'
+      ],
+      datasets: [{
+        label: 'Usage%',
+        data: [80, 20],
+        backgroundColor: [
+          'rgba(220, 53, 69, 1.0)',
+          'rgba(185, 185, 185, 1.0)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Historical Memory 15 min
+  const ctx6 = document.getElementById('minMemoryUsage');
+  
+  new Chart(ctx6, {
+    type: 'pie',
+    data: {
+      labels: [
+        '15 min avg. Memory Usage'
+      ],
+      datasets: [{
+        label: 'Usage%',
+        data: [65, 45],
+        backgroundColor: [
+          'rgba(253, 126, 20, 0.85)',
+          'rgba(185, 185, 185, 0.85)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Historical Memory 1 hr
+  const ctx7 = document.getElementById('hourMemoryUsage');
+  
+  new Chart(ctx7, {
+    type: 'pie',
+    data: {
+      labels: [
+        '1 hr avg. Memory Usage'
+      ],
+      datasets: [{
+        label: 'Usage%',
+        data: [45, 55],
+        backgroundColor: [
+          'rgba(255, 193, 7, 0.7)',
+          'rgba(185, 185, 185, 0.7)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Historical Memory 1 day
+  const ctx8 = document.getElementById('dayMemoryUsage');
+  
+  new Chart(ctx8, {
+    type: 'pie',
+    data: {
+      labels: [
+        '1 day avg. Memory Usage'
+      ],
+      datasets: [{
+        label: 'Usage%',
+        data: [15, 85],
+        backgroundColor: [
+          'rgba(25, 135, 84, 0.55)',
+          'rgba(185, 185, 185, 0.55)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Current Storage
+  const ctx9 = document.getElementById('currentStorageUsage');
+  
+  new Chart(ctx9, {
+    type: 'pie',
+    data: {
+      labels: [
+        'Current Storage Usage'
+      ],
+      datasets: [{
+        label: 'Usage%',
+        data: [30, 70],
+        backgroundColor: [
+          'rgba(255, 193, 7, 1.0)',
+          'rgba(185, 185, 185, 1.0)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Historical Storage 15 min
+  const ctx10 = document.getElementById('minStorageUsage');
+  
+  new Chart(ctx10, {
+    type: 'pie',
+    data: {
+      labels: [
+        '15 min avg. Storage Usage'
+      ],
+      datasets: [{
+        label: 'Usage%',
+        data: [15, 85],
+        backgroundColor: [
+          'rgba(25, 135, 84, 0.85)',
+          'rgba(185, 185, 185, 0.85)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Historical Storage 1 hr
+  const ctx11 = document.getElementById('hourStorageUsage');
+  
+  new Chart(ctx11, {
+    type: 'pie',
+    data: {
+      labels: [
+        '1 hr avg. Storage Usage'
+      ],
+      datasets: [{
+        label: 'Usage%',
+        data: [40, 60],
+        backgroundColor: [
+          'rgba(255, 193, 7, 0.7)',
+          'rgba(185, 185, 185, 0.7)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  // Historical Storage 1 day
+  const ctx12 = document.getElementById('dayStorageUsage');
+  
+  new Chart(ctx12, {
+    type: 'pie',
+    data: {
+      labels: [
+        '1 day avg. Storage Usage'
+      ],
+      datasets: [{
+        label: 'Usage%',
+        data: [15, 85],
+        backgroundColor: [
+          'rgba(25, 135, 84, 0.55)',
+          'rgba(185, 185, 185, 0.55)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  
+  
+  
+  
+  
+  
+  
+  /* const data = [];
   data[0] = hostname;
   data[1] = cpuModel;
   data[2] = cpuLoad;
   
-  writeMetrics(data);
+  writeMetrics(data); */
   
 };
 
